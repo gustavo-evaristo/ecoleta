bc83a97ba8e91650ff0b211028856d08f2ed2c72
Gustavo <gug.henri1@gmail.com>
2020-12-17 10:49:29 -0300

    Alterações

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,53 @@
 import express from 'express'
 
+import Knex from './database/connection'
+
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-    return res.send('ok')
+routes.get('/items', async (req, res) => {
+    const items = await Knex('items').select('*')
+
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`
+        }
+    })
+
+    return res.json(serializedItems)
+})
+
+routes.post('/points', async (req, res) => {
+    const { name, email, whatsapp, latitude, longitude, city, uf, items } = req.body
+
+    try {
+        const insertedItems = await Knex('points').insert({
+            image: 'image-fake',
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf
+        })
+    
+        const point_id = insertedItems[0]
+
+        const pointItems = items.map((item_id: number) => {
+            return {
+                item_id,
+                point_id
+            }
+        }) 
+
+        await Knex('points_items').insert(pointItems)
+    
+        return res.sendStatus(200)
+    } catch (err) {
+        return console.error(err)
+    }
 })
 
 export default routes
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from 'express'
 
 import routes from './routes'
 
+import path from 'path'
+
 const app = express()
 
 app.use(express.json())
 
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 app.use(routes)
 
-app.listen(3333, () => {
-    return console.log('acessou')
-})
+app.listen(3333)
